481359b66b054f18ffd994a1b6d279770cf0456c
Francisco Javier Carrera Arias <francarrera13@gmail.com>
2018-10-24 09:28:39 -0400

    Update BandPowers.m

--- BandPowers.m
+++ BandPowers.m
@@ -40,9 +40,9 @@ for i = 1:5 % Compute band powers for each EEG channel
 [spectra,freqs] = spectopo(EEG.data(i,:,:), 0, EEG.srate, 'plot', 'off');
 
 % theta=4-7, alpha=8-12, beta=13-30
-thetaFreq = find(freqs>=4 & freqs<=7);
+thetaFreq = find(freqs>=4 & freqs<=8);
 alphaFreq = find(freqs>=8 & freqs<=12);
-betaFreq  = find(freqs>=13 & freqs<=30);
+betaFreq  = find(freqs>=12 & freqs<=30);
 
 % compute spectral power
 thetaPow = [thetaPow,10^(mean(spectra(thetaFreq))/10)];
